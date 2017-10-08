8287332094d2eef77284d2d59f7bfaf0030be09f
jdsnyderii <jdsnyderii@gmail.com>
2017-10-08 18:59:56 -0500

    Add some graphml tests and some JsonSchema tests.

--- src/test/java/org/jim/JsonSchemaTest.java
+++ src/test/java/org/jim/JsonSchemaTest.java
@@ -1,37 +1,50 @@
 package org.jim;
 
+import junit.framework.TestCase;
 import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class JsonSchemaTest {
 
   @Test
   public void testSchema() throws Exception {
-    
+
     InputStream inputStream = getResourceAsStream("json-schema/degree-planning.json");
 
     JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
     SchemaLoader schemaLoader = SchemaLoader.builder()
         .schemaJson(rawSchema)
-        .resolutionScope("http://jim.org/") // setting the default resolution scope
         .build();
-    
+
     Schema schema = schemaLoader.load().build();
 
     JSONObject plan = new JSONObject(new JSONTokener(getResourceAsStream("json-schema-files/plan1.json")));
-    
+
     try {
       schema.validate(plan);
     } catch (ValidationException e) {
-      System.out.println(e.getMessage());
-      e.getCausingExceptions().stream().map(ValidationException::getMessage).forEach(System.out::println);
+      logValidationException(e);
+      TestCase.fail(e.getMessage());
     }
 
+  }
+
+  private void logValidationCausingExceptions(List<ValidationException> exceptions) {
+    exceptions.forEach(this::logValidationException);
+  }
+
+  private void logValidationException(ValidationException e) {
+    System.out.println(e.getMessage());
+    if (e.getCausingExceptions().size() > 0) {
+      logValidationCausingExceptions(e.getCausingExceptions());
+    }
   }
 
   private InputStream getResourceAsStream(String name) throws Exception {
