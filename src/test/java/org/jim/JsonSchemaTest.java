2bed5b566c34f8cecd3a47d33da73a61c8855861
jdsnyderii <jdsnyderii@gmail.com>
2017-10-08 00:18:02 -0500

    Add some graphml tests and some JsonSchema tests.

--- src/test/java/org/jim/JsonSchemaTest.java
+++ src/test/java/org/jim/JsonSchemaTest.java
@@ -1,11 +1,28 @@
 package org.jim;
 
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 public class JsonSchemaTest {
 
   @Test
-  public void testSchema() {
+  public void testSchema() throws Exception {
 
+    InputStream inputStream = getResourceAsStream("json-schema/degree-planning.json");
+    JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
+    Schema schema = SchemaLoader.load(rawSchema);
+
+    JSONObject plan = new JSONObject(new JSONTokener(getResourceAsStream("json-schema-files/plan1.json")));
+    schema.validate(plan); // throws a ValidationException if this object is invalid
+
+  }
+
+  private InputStream getResourceAsStream(String name) throws Exception {
+    return getClass().getClassLoader().getResourceAsStream(name);
   }
 }
